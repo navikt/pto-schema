6e568fa24c233ba1ddd63cda736cae08dcb5fb88
Kristina Skatun <36476370+kcs85@users.noreply.github.com>
2025-09-29 13:35:37 +0200

    Lag ny versjon av veileder tilordnet (#9)

---
+++ src/main/java/no/nav/pto_schema/kafka/json/topic/onprem/VeilederTilordnetV2.java
@@ -0,0 +1,18 @@
+package no.nav.pto_schema.kafka.json.topic.onprem;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.Value;
+
+import java.time.ZonedDateTime;
+
+@Value
+@NoArgsConstructor(force = true)
+@AllArgsConstructor
+@Builder
+public class VeilederTilordnetV2 {
+    String aktorId;
+    String veilederId;
+    ZonedDateTime tilordnetTidspunkt;
+}
